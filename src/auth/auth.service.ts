b6e33f13c7b415dbf71ab68d045cc96bc7586e6d
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-13 18:27:40 +0200

    implement authorization with jwt token and passport library

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayloadI } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async singUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ where: { username } });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (user && isPasswordValid) {
+      const payload: JwtPayloadI = { username };
+      const accessToken: string = this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from './users.repository';
+import { JwtPayloadI } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usersRepository: UsersRepository) {
+    super({
+      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayloadI): Promise<User> {
+    const { username } = payload;
+    const user = await this.usersRepository.findOne({ where: { username } });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
